89bc5d03ceeaabd51a6e1722de2085edc54e388b
JANICE GATHOGA <judysjewel17@gmail.com>
2022-09-12 16:46:42 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,19 +5,14 @@
 
 
 /**
+ * main - program that prints the alphabet in lowercase, followed by a new line
  *
- *  * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
  *
- *   * You can only use the putchar
+ * Return: 0
  *
- *    * Return: 0
- *
- *     */
-
-
-
+*/
 int main(void)
-
 {
 
 	int l = 'a';
