611e6531d76f51cc4d7863db4e73f770d161a657
JANICE GATHOGA <judysjewel17@gmail.com>
2022-09-12 17:45:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,6 @@
 
 
  /**
-
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
  *
@@ -13,11 +12,8 @@ int main(void)
 {
 
 		write(2,
-
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-
-					      	      59);
-
+				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		     59);
 
 
 			return (1);
