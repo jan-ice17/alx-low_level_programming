50742637286ad3483f25ea84672342ef009c2e83
Janice Gathoga <janicegathoga@gmail.com>
2023-11-20 00:39:06 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+	#include <stdlib.h>
+	#include <ctype.h>
+	#include <string.h>
 /**
- * main - Program that multiplies two numbers.
- * @argc: This is the argument count
- * @argv: This is the argument vector
- *
- * Return: 0;
+ * main - Prints coints left
+ * @args: Paramater Entry
+ * @argv: One number of argumets
+ * Return: zero onOne number of argumetsr
  */
-int main(int argc, char *argv[])
-{
-	int index, multiplication;
 
-	multiplication = 1;
-	if (argc < 3)
+int main(int args, char *argv[])
+{
+	int  c, coins = 0;
+
+	if (args != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	for (index = 1; index < argc; index++)
+	c = atoi(argv[1]);
+	if (c < 0)
 	{
-		multiplication = multiplication * atoi(argv[index]);
+		printf("0\n");
+		return (0);
 	}
-	printf("%d\n", multiplication);
+	for (; c >= 0;)
+	{
+		if (c >= 25)
+			c -= 25;
+
+		else if (c >= 10)
+			c -= 10;
+
+		else if (c >= 5)
+			c -= 5;
+
+		else if (c >= 2)
+			c -= 2;
+
+		else if (c >= 1)
+			c -= 1;
+		else
+			break;
+		coins += 1;
+	}
+	printf("%d\n", coins);
 	return (0);
 }
