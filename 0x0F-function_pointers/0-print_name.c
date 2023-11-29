f94d6c9af4910ffde060ed8db9c90199998881c4
Janice Gathoga <janicegathoga@gmail.com>
2023-11-29 12:06:08 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
 *print_name --the func prints a name
 *@name--name which print
-*@f--function which print name
+*@f:-function which print name
 */
 
 void print_name(char *name, void (*f)(char *))
