c4fd4cba3abc4eca42b20b53e02aa8733a983c19
Janice Gathoga <janicegathoga@gmail.com>
2023-12-14 12:25:26 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,15 @@
+#include "main.h"
 
+/**
+ * print_binary - printsbinary representation of a number
+ * @n: number to convert
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+
+}
